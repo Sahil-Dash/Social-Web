e9c7818a838b2fc3ecc3edff37440336344057f5
Sahil-Dash <sahildash386@gmail.com>
2024-07-15 12:33:13 +0530

    added vercel urls to allowed hosts

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -26,11 +26,8 @@ SECRET_KEY = 'django-insecure--65hwsh7^-+@-tr8c(fnwcky0-yek4r15&y%pf1td2pfss62m-
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'http://localhost:5174',
-    'http://localhost:5173',
-    "http://localhost:3000",
-    "https://social-web-server.vercel.app/", 
-    "https://snapgram-six-rouge.vercel.app",
+    "social-web-server.vercel.app",
+    "snapgram-six-rouge.vercel.app"
 ]
 
 
